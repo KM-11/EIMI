4ac5290690bce34799c5fcc72d93906f86344512
josE4roldan <josalbacet@gmail.com>
2019-11-28 12:20:20 +0100

    Añadida vista global de muestras

--- web/views.py
+++ web/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
-
+from .models import Muestra
+from django.db.models import Count
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def samples_detail(request):
+    cuenta_familia = Muestra.objects.all().values('familia').annotate(total=Count('familia')).order_by('total')
+
+    latest_muestra_list =Muestra.objects.order_by('-date')[:5]
+    context = {'latest_muestra_list': latest_muestra_list,
+               'cuenta_familia': cuenta_familia}
+    return render(request, 'web/global_samples.html', context)
