b8635445f3296d652ac81ad6aa729282c376871f
josE4roldan <josalbacet@gmail.com>
2019-11-28 11:55:57 +0100

    Se añaden rutas

--- EIMI/urls.py
+++ EIMI/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from web import views
 
 urlpatterns = [
+
     path('', views.index),
     path('admin/', admin.site.urls),
+    path('EIMI/', include("web.urls")),
 
 ]
--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 
 urlpatterns = [
-    path('eimi_p/',include("eimidb.urls")),
-    path('admin/', admin.site.urls),
+    path('',views.index),
+
 ]
