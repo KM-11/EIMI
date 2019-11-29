1f17f3be2e628e740b53c61207dacd3e71064f65
Javier Carrillo Mondejar <maximo@MacBook-Pro.mshome.net>
2019-11-29 15:59:48 +0100

    Find ip address

--- core/helper.py
+++ core/helper.py
@@ -1,7 +1,20 @@
 import dotenv
 from pathlib import Path
-
+import re 
+import ipaddress
 
 def load_env_file():
     env_path = Path('..') / '.env'
     dotenv.load_dotenv(dotenv_path=env_path)
+
+def is_ip_valid(ip):
+	try:
+		ipaddress.ip_address(ip)
+		return True
+	except:
+		return False
+		
+def find_ip_address(data):
+	pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+	matches = re.findall(pattern,data)
+	return list(filter(lambda x: is_ip_valid(x),list(matches)))
