5db79dfee78255d0bcca9c373a48ac36ad714da0
josE4roldan <josalbacet@gmail.com>
2019-11-29 11:55:07 +0100

    Detector syscall/param v1
    detecta y cuenta las combinaciones syscall/param dado un fichero con  las syscall y parametros a encontrar

--- core/dynamic_analyzer.py
+++ core/dynamic_analyzer.py
@@ -1,8 +1,8 @@
 
 from nltk import ngrams
 import re
-
-
+import os
+from helper import load_env_file
 from functools import reduce
 
 def main():
@@ -10,6 +10,29 @@ def main():
 
 def jaccard_coef(list1, list2,k1,k2):
     return list(map(lambda x: len(set(ngrams(list1,x)).intersection(set(ngrams(list2,x))))/len(set(ngrams(list1,x)).union(set(ngrams(list2,x)))),range(k1,k2+1)))
+
+
+def look_write_files(syscall_traza):
+    load_env_file()
+    suspects_file = open(os.getenv('SUSPECT_FILES_PATH'),"r")
+    detects=[]
+    for tup in suspects_file.readlines():
+       detects.append( tup.rstrip().split(",")[0],tup.rstrip().split(",")[1])
+    resultcount=[]
+    counter=0
+    for i in detects:
+        for ssys in syscall_traza:
+            if(i[0]==ssys["syscall"]):
+                for param in ssys["params"]:
+                    if(i[1] in param):
+                        counter=counter+1
+        resultcount.append({i[0]:counter})
+        counter=0
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
