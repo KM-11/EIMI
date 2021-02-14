0d3436123d273d17f7c83bd41c57d91adc0a07e3
Dankitani <dankitan@users.noreply.github.com>
2021-02-14 20:25:53 +0100

    Update admin.py

--- web/admin.py
+++ web/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Vmachine)
-admin.site.register(Sample)
+admin.site.register(Muestra)
 admin.site.register(Architecture)
-admin.site.register(Family)
+admin.site.register(Familia)
